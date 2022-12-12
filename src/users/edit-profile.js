539a453f209398d04cbf8fef56f794981fb4230d
smitdar21 <smit@pointfocal.com>
2022-12-12 18:35:44 -0500

    login reg and profile done

---
+++ src/users/edit-profile.js
@@ -0,0 +1,24 @@
+
+import React from "react";
+import {Link, useNavigate} from "react-router-dom";
+import "./profile.css";
+
+
+const editProfile = () => {
+
+
+
+
+
+    }
+    return(
+        <>
+            {
+                currentUser &&
+                <h2>Edit Profile</h2>
+            }
+
+        </>
+    )
+}
+export default editProfile
